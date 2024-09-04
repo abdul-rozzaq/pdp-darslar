ef842da07decb9de74d4263ef279dbc2c44226f5
Abdurazzoq Abdusalomov <120030195+abdul-rozzaq@users.noreply.github.com>
2024-09-04 23:38:40 +0500

    Update 11.cpp | correct

--- 2-modul/7-dars/11.cpp
+++ 2-modul/7-dars/11.cpp
@@ -24,14 +24,14 @@ vector<int> squareUp(int n)
 
     for (int i = 1; i <= n; i++)
     {
+
         for (int j = 1; j <= i; j++)
         {
-            
+            result[i * n - j] = j;
         }
+        
     }
     
-
-
     return result;
 }
 
